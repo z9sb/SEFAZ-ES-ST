6225a0d11fb511b24ec16bc81ff9c3991a4eb060
z9sb <rayslansantosnatista@gmail.com>
2023-11-03 23:52:22 -0300

    Add new function (search in e-mail)

---
+++ email.py
@@ -0,0 +1,66 @@
+import imaplib
+import email
+from email.header import decode_header
+from main import main
+from datetime import datetime
+
+
+#diretório para download
+root_dir = r"C:\\Users\\raysl_3a68bgu\\OneDrive\\Documentos\\Python\\outlook\\Arquivos\\"
+
+# Configurações do servidor IMAP do Outlook.com
+username = "contabilidadebento.xml@hotmail.com"
+password = "xml1266@"
+server = "imap-mail.outlook.com"
+
+# Conectando-se ao servidor IMAP
+mail = imaplib.IMAP4_SSL(server)
+mail.login(username, password)
+
+# Selecionando a caixa de entrada
+mail.select("inbox")
+
+# Pesquisando emails com anexos
+status, messages = mail.search(None, "ALL")
+messages = list(reversed(messages[0].split()))
+
+for mail_id in messages:
+    _, msg_parts = mail.fetch(mail_id, "(RFC822)")
+    msg = email.message_from_bytes(msg_parts[0][1])
+    subject, encoding = decode_header(msg["Subject"])[0]
+    print(encoding)
+    if isinstance(subject, bytes):
+        subject = subject.decode(encoding or "utf-8")
+    from_ = msg.get("From")
+    date = msg.get("Date")
+
+    print(f"Subject: {subject}")
+    print(f"From: {from_}")
+    print(f"Date: {date}")
+    if 'UTC' in date:
+        date_email = datetime.strptime(date, "%a, %d %b %Y %H:%M:%S %z (UTC)")
+    else:
+        date_email = datetime.strptime(date, "%a, %d %b %Y %H:%M:%S %z")
+    
+    if date_email.month != datetime.now().month:
+        break
+    
+    # Verificando anexos
+    for part in msg.walk():
+        if part.get_content_maintype() == "multipart":
+            continue
+        if part.get("Content-Disposition") is None:
+            continue
+
+        filename = part.get_filename()
+        if filename:
+            print(f"Anexo: {filename}")
+            try:
+                with open(f"{root_dir}{filename}", "wb") as f:
+                    f.write(part.get_payload(decode=True))
+            except:
+                pass
+# Fechando a conexão
+mail.close()
+mail.logout()
+main(root_dir)
--- main.py
+++ main.py
@@ -3,17 +3,41 @@ from pathlib import Path
 from easygui import diropenbox #type: ignore 
 from request_sefaz import Api
 from requests import Session  # type: ignore
+from patoolib import extract_archive # type: ignore
+from tempfile import TemporaryDirectory
+from shutil import copy2
 
-url_html = 'http://www2.sefaz.es.gov.br/LegislacaoOnline/lpext.dll/InfobaseLegislacaoOnline/portarias/2019/port16-r%20-%20atualizada.htm?f=templates&fn=document-frame.htm&2.0'
-html_content = Session().get(url_html)
+class main():
+    def __init__(self, root_dir):
+        self.url_html = 'http://www2.sefaz.es.gov.br/LegislacaoOnline/lpext.dll/InfobaseLegislacaoOnline/portarias/2019/port16-r%20-%20atualizada.htm?f=templates&fn=document-frame.htm&2.0'
+        self.html_content = Session().get(self.url_html)
+        self.root_dir = root_dir
+        self.file_xml: list = []
+        self.xml_dir()
+        
+    def rar_extract(self, arquivo):
+        with TemporaryDirectory() as temp_dir:
+            try:
+                extract_archive(arquivo, outdir=temp_dir)
+            except:
+                pass    
+            
+            for pasta_atual, subpastas, arquivos in walk(temp_dir):
+                for arquivo in arquivos:
+                    if arquivo.endswith('.xml'):
+                        self.file_xml.append(path.join(pasta_atual, arquivo))
+                        copy2(path.join(pasta_atual, arquivo), r"C:\Users\raysl_3a68bgu\OneDrive\Documentos\Python\outlook\teste2\\")
+    def xml_dir(self):      
+        for pasta_atual, subpastas, arquivos in walk(self.root_dir):
+            for arquivo in arquivos: 
+                if arquivo.endswith('.xml'): 
+                    self.file_xml.append(path.join(pasta_atual, arquivo))
+                elif arquivo.endswith('.zip') or arquivo.endswith('.rar'):
+                    self.rar_extract(path.join(pasta_atual, arquivo))
 
-rootdir = diropenbox(default= 'C:\donwload\*.xml')
-root_dir = Path(rootdir)
+        Api(self.html_content, self.file_xml)
 
-file_xml = []
-for pasta_atual, subpastas, arquivos in walk(root_dir):
-    for arquivo in arquivos: 
-        if arquivo.endswith('.xml'): 
-            file_xml.append(path.join(pasta_atual, arquivo))
-
-Api(html_content, file_xml)
+if __name__ == '__main__': 
+    rootdir = diropenbox(default= 'C:\donwload\*.xml')
+    root_dir = Path(rootdir)
+    main(root_dir)
